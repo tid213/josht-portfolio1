52614fba7ada3063211be5a258860f3a7948f7ff
tid213 <tid2136141@gmail.com>
2023-02-15 18:13:13 -0700

    fixed jquery to change main text on project click

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 
+$(document).ready(function () {
+    $('.project-1-link').click(function(){
+        console.log("clicked");
+        //Remove selected from all
+        $(".main-div").removeClass("selected");
+        $(".main-div").addClass("unselected");
+        //Add selected to clicked item
+        $('.project-1-info').removeClass("unselected");
+        $('.project-1-info').addClass("selected");
 
-$('#clicked').click(function(){
-    console.log("clicked");
-    //Remove selected from all
-    $(".main-div").removeClass("selected");
-    $(".main-div").addClass("unselected");
-    //Add selected to clicked item
-    $('.project-1-info').addClass("selected");
-
-})
+    })
+    $('.project-2-link').click(function(){
+        console.log("clicked");
+        //Remove selected from all
+        $(".main-div").removeClass("selected");
+        $(".main-div").addClass("unselected");
+        //Add selected to clicked item
+        $('.project-2-info').removeClass("unselected");
+        $('.project-2-info').addClass("selected");
+    
+    })
+    $('.project-3-link').click(function(){
+        console.log("clicked");
+        //Remove selected from all
+        $(".main-div").removeClass("selected");
+        $(".main-div").addClass("unselected");
+        //Add selected to clicked item
+        $('.project-3-info').removeClass("unselected");
+        $('.project-3-info').addClass("selected");
+        
+    })
+});
